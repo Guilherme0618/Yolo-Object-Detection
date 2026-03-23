985bf43d14a5e9696c4107b574aecefb3c45761f
Guilherme0618 <guibussadori1@gmail.com>
2026-03-23 19:41:25 -0300

    V1.0 Yolo detection

---
+++ src/webcam.py
@@ -0,0 +1,35 @@
+from ultralytics import YOLO
+import cv2
+
+def main():
+    # TESTE 
+    # model = YOLO("best.pt")
+
+    # TESTE  2
+    model = YOLO("yolov8n.pt")
+
+    cap = cv2.VideoCapture(0)
+
+    if not cap.isOpened():
+        print("Erro ao abrir webcam")
+        return
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        results = model(frame, conf=0.25, device="cpu")
+
+        annotated = results[0].plot()
+
+        cv2.imshow("YOLO Webcam", annotated)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
