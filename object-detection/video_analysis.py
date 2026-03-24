41b14c0083621ae16ef7bbb8003e4161e93bd935
Guilherme0618 <guibussadori1@gmail.com>
2026-03-24 13:07:46 -0300

    add: new model + video_analysis(vehicle)

---
+++ object-detection/video_analysis.py
@@ -0,0 +1,21 @@
+from ultralytics import YOLO
+import cv2
+
+model = YOLO("src/models/CarsDetection.pt")
+cap = cv2.VideoCapture("videos/video2.mp4")
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    results = model(frame, conf=0.20)
+    annotated = results[0].plot()
+
+    cv2.imshow("Modelo pronto", annotated)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
